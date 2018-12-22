799260583241a77d1866e46c4448a629e1941630
SUNGBUMPARK <psbum1609@gmail.com>
2018-12-22 20:44:29 +0900

    Update version

--- npmtest-codemcd.js
+++ npmtest-codemcd.js
@@ -1,92 +1,78 @@
-// (function () {
-//     var q = function () {
-        
-//     };
-
-//     exports = module.exports = q;
-
-//     var enq = _enqueue(dataStore, element) {
-//         dataStore.push(element);
-//     };
-//     q.enqueue = function(dataStore, element) {
-//         _enqueue(dataStore, element);
-//     };
-// }());
+var dataStore = [];
+var q = function () {
+    this.enqueue = enqueue;
+    this.dequeue = dequeue;
+    this.front = front;
+    this.back = back;
+    this.toString = toString;
+    this.empty = empty;
+    this.size = size;
+}
 
 exports = module.exports = q;
-var q = function() {
-    this.dataStore = [];
-    this.enqueue = _enqueue;
-    this.dequeue = _dequeue;
-    this.front = _front;
-    this.back = _back;
-    this.toString = _toString;
-    this.empty = _empty;
-    this.size = _size;
-}
 
 function _enqueue(element) {
-    this.dataStore.push(element);
+    dataStore.push(element);
 }
 
-q.enqueue = function(element) {
+q.enqueue = function (element) {
     _enqueue(element);
 }
 
 function _dequeue() {
-    return this.dataStore.shift();
+    return dataStore.shift();
 }
 
-q.dequeue = function() {
+q.dequeue = function () {
     return _dequeue();
 }
 
 function _front() {
-    return this.dataStore[0];
+    return dataStore[0];
 }
 
-q.front = function() {
+q.front = function () {
     return _front();
 }
 
 function _back() {
-    return this.dataStore[this.dataStore.length-1];
+    return dataStore[dataStore.length - 1];
 }
 
-q.back = function() {
+q.back = function () {
     return _back();
 }
 
 function _toString() {
     var retStr = "";
-    for(var i = 0; i < this.dataStore.length; ++i) {
-        retStr += this.dataStore[i] + "\n";
+    for (var i = 0; i < dataStore.length; ++i) {
+        retStr += dataStore[i] + "\n";
     }
 
     return retStr;
 }
 
-q.toString = function() {
+q.toString = function () {
     return _toString();
 }
 
 function _empty() {
-    if(this.dataStroe.length == 0) {
+    if (dataStroe.length == 0) {
         return true;
     }
-    else{
+    else {
         return false;
     }
 }
 
-q.empty = function() {
+q.empty = function () {
     return _empty();
 }
 
 function _size() {
-    return this.dataStore.length;
+    return dataStore.length;
 }
 
-q.size = function() {
+q.size = function () {
     return _size();
 }
